676c47274f7d61f7cc6ab2a52204c1bca0ac0e6b
Drew Bradley <williamandrewb@gmail.com>
2020-10-15 21:17:57 -0600

    Add random dog poster option

--- main.js
+++ main.js
@@ -22,25 +22,72 @@ var quotesArray = [
     "cat's are worthy of love and respect."
 ]
 
-var button = document.querySelector(".button");
+var pupImagesArray = [
+    '/Users/drewbradley/interactive-practice/pics/pup1.jpg',
+    '/Users/drewbradley/interactive-practice/pics/pup2.jpg',
+    '/Users/drewbradley/interactive-practice/pics/pup3.jpg',
+    '/Users/drewbradley/interactive-practice/pics/pup4.jpg',
+    '/Users/drewbradley/interactive-practice/pics/pup5.jpg'
+];
+
+var pupTitleArray = [
+    "LOOK AT THAT PUPPERS!",
+    "COME GET THIS DOGGO!",
+    "I LOVE PUPS SO MUCH!",
+    "YOU SHOULD BE A DOGGO!",
+    "WOOF! WOOF! DOG! DOG!"
+];
+
+var pupQuotesArray = [
+    "who let the dogs out?",
+    "dogs are man's best friend",
+    "your dog misses you",
+    "na na na na na it's the D-O-G!",
+    "dog say bork bork bork."
+]
+
+var button = document.querySelector('.button');
+var pupButton = document.querySelector('.pup-button');
 
 function randomIndex(array){
     return Math.floor(Math.random() * array.length);
 };
 
 var mainFrame = document.querySelector('.main-frame');
+var pupperMain = document.querySelector('.puppy-main')
 
-function workDammit() {
+function catShuffle() {
     var randomCatPoster = {
         img: imagesArray[randomIndex(imagesArray)],
         title: titleArray[randomIndex(titleArray)],
         quote: quotesArray[randomIndex(quotesArray)]
     };
     mainFrame.innerHTML = 
-`<img class="image" src="${randomCatPoster.img}" alt="">
-<h1 class = 'title'>${randomCatPoster.title}</h1>
-<p class="paragraph">${randomCatPoster.quote}</p>`;
-console.log('AAAHHHHHHHH!!!!!')
+        `<img class="image" src="${randomCatPoster.img}" alt="">
+        <h1 class = 'title'>${randomCatPoster.title}</h1>
+        <p class="paragraph">${randomCatPoster.quote}</p>`;
+    mainFrame.style.display = 'block';
+    pupperMain.style.display = 'none';
 }
-workDammit();
-button.addEventListener("click", workDammit);
+
+function dogShuffle() {
+    var randomDogPoster = {
+        img: pupImagesArray[randomIndex(pupImagesArray)],
+        title: pupTitleArray[randomIndex(pupTitleArray)],
+        quote: pupQuotesArray[randomIndex(pupQuotesArray)]
+    };
+    pupperMain.innerHTML = 
+        `<img class="image" src="${randomDogPoster.img}" alt="">
+        <h1 class = 'title'>${randomDogPoster.title}</h1>
+        <p class="paragraph">${randomDogPoster.quote}</p>`;
+    mainFrame.style.display = 'none';
+    pupperMain.style.display = 'block';
+}
+
+
+catShuffle();
+button.addEventListener("click", catShuffle);
+
+
+dogShuffle();
+pupButton.addEventListener("click", dogShuffle);
